6226a20fa706a23069c125a9557b645d1bfb165a
boyutov <boytov97@gmail.com>
2025-02-09 16:06:33 +0600

    -feat: Base data types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+int age = 45;
+string name = "A";
+bool isActive = false;
+decimal size = 178;
+float weight = 789;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(age);
+Console.WriteLine(name);
+Console.WriteLine(isActive);
+Console.WriteLine(size);
+Console.WriteLine(weight);
--- obj/Debug/net9.0/30github.AssemblyInfo.cs
+++ obj/Debug/net9.0/30github.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("30github")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6570e9fca06473dc451577b133d6cee999fa1f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("30github")]
 [assembly: System.Reflection.AssemblyTitleAttribute("30github")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
