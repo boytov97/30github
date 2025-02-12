1f2034fcb62cde5fd5166c63581810c57abca58a
boyutov <boytov97@gmail.com>
2025-02-12 07:59:05 +0600

    -feat: Namespace and loop

--- Program.cs
+++ Program.cs
@@ -1,32 +1,64 @@
-﻿
-int age = 45;
-string name = "A";
-bool isActive = false;
-decimal size = 178;
-float weight = 789;
-string? lastName = null;
-char symbol = 'd';
+﻿using System;
 
-float someValue = 4.56f;
-someValue = 67.5f;
+namespace project
+{
 
-double otherValue = -786782.78f;
+    class Program
+    {
 
-short height = -30678;
-byte position = 255;
-long bigNumber = 9223372036854775807;
+        static void Main()
+        {
+            Console.Write("Перевод строки – \n\n");
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(age);
-Console.WriteLine(name);
-Console.WriteLine(isActive);
-Console.WriteLine(size);
-Console.WriteLine(weight);
-Console.WriteLine(lastName);
-Console.WriteLine(someValue);
-Console.WriteLine(height);
-Console.WriteLine(position);
-Console.WriteLine(bigNumber);
-Console.WriteLine(otherValue);
-Console.WriteLine(symbol);
+            // Строчный комментарий
+            /*
+                Огромный
+                комментарий!
+            */
+            // Console.Write("Комментарий");
+            Console.Write("Текст");
+
+            int age = 45;
+            string name = "A";
+            bool isActive = false;
+            decimal size = 178;
+            float weight = 789;
+            string? lastName = null;
+            char symbol = 'd';
+
+            float someValue = 4.56f;
+            someValue = 67.5f;
+
+            double otherValue = -786782.78f;
+
+            short height = -30678;
+            byte position = 255;
+            long bigNumber = 9223372036854775807;
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+            Console.WriteLine(isActive);
+            Console.WriteLine(size);
+            Console.WriteLine(weight);
+            Console.WriteLine(lastName);
+            Console.WriteLine(someValue);
+            Console.WriteLine(height);
+            Console.WriteLine(position);
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(otherValue);
+            Console.WriteLine(symbol);
+
+            Console.WriteLine(position - age);
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(i);
+
+            // Получение данных
+            Console.ReadLine();
+        }
+
+    }
+
+}
